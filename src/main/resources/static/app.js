eb149dbe8ded9c13dc95fd3c968c25d134e2dcd0
Homeshevchuk <bshevchuk1@gmail.com>
2016-10-05 18:52:07 +0300

    Reg

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,20 +1,29 @@
- var scotchApp = angular.module('scotchApp', []);
+ var scotchApp = angular.module('scotchApp', ['ui.bootstrap']);
     scotchApp.controller('mainController', function($scope,$http) {
-        $scope.counter = 0;
-        $scope.user = {
-            name:'',
-            password:''
-        }
-        $scope.send = function () {
+        $http({
+            method: 'GET',
+            url: '/api/user',
+            headers: {
+                'X-Requested-With': 'XMLHttpRequest'
+            }
+        }).then(function successCallback(response) {
+            $scope.authenticated = true;
+        }, function errorCallback(response) {
+            $scope.authenticated = false;
+        });
+        $scope.auth = function () {
+            var auth = btoa($scope.user.name+':'+$scope.user.password);
             $http({
-                method: 'POST',
-                url: '/registration',
-                data: $scope.user
+                method: 'GET',
+                url: '/api/user',
+                headers: {
+                    'Authorization':'Basic '+auth,
+                    'X-Requested-With': 'XMLHttpRequest'
+                }
             }).then(function successCallback(response) {
                 console.log(response);
             }, function errorCallback(response) {
                 console.log(response);
             });
         }
-
     });
